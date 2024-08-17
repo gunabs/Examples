9c9472e69f82c027b624d89e53aeb0b8000a940e
guna09bs <guna09bs@gmail.com>
2024-08-17 19:32:17 +0530

    problem

--- src/LeedCode_problem/Two_Sum.java
+++ src/LeedCode_problem/Two_Sum.java
@@ -3,8 +3,36 @@ package LeedCode_problem;
 public class Two_Sum {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Two_Sum obj=new Two_Sum();
+		
+		int nums[]= {2,7,11,15}; 
+		
+		int target=7;
+		
+		obj.twoSum(nums, target);
+		
 
 	}
+	
+	 public int[] twoSum(int[] nums, int target) {
+			
+		  
+			for(int i = 0;i<nums.length;i++){ 
+				for(int j = i+1;j<nums.length;j++){
+        
+					 if(nums[i] + nums[j] == target){
+           
+                      return new int[]{i,j};
+                     }
+                 
+				}
+
+			
+         }
+     
+     return new int[]{};
+		
+ 
+ }
 
 }
