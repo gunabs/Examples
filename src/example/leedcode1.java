525be9a57f81a8acb88283687cc4517863d1468c
guna09bs <guna09bs@gmail.com>
2024-08-17 11:57:01 +0530

    second commit

---
+++ src/example/leedcode1.java
@@ -0,0 +1,15 @@
+package example;
+
+public class leedcode1 {
+
+	public static void main(String[] args) {
+		int a=1;
+		int b=10;
+		int c=a+b;
+		
+		
+		System.out.println("cal c="+c);
+
+	}
+
+}
